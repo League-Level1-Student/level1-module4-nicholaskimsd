dc950fdc5cd1e77920d0123bd87596708b31c02c
League Student <league@iMac-8.attlocal.net>
2019-07-14 17:29:31 -0700

    i am mostly done with typewriter

--- src/Typewriter.java
+++ src/Typewriter.java
@@ -1,72 +1,68 @@
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.omg.CORBA.Current;
 
 public class Typewriter implements KeyListener {
 	JFrame frame = new JFrame();
 	JLabel label = new JLabel();
+	JPanel panel = new JPanel();
+
 	
-public static void main(String[] args) {
-	
-}
 
-
-
-
-char generateRandomLetter() {
-    Random r = new Random();
-    return (char) (r.nextInt(26) + 'a');
-}
-
-
-
-
-public void runner() {
-	frame.add(label);
-	frame.setVisible(true);
-	char hi = generateRandomLetter();
-	label.setText(Character.toString(hi));
-	frame.addKeyListener(this);
-	label.setFont(label.getFont().deriveFont(28.0f));
-	label.setHorizontalAlignment(JLabel.CENTER);
-	frame.pack();
-	
-}
-
-
-
-
-@Override
-public void keyTyped(KeyEvent e) {
-	// TODO Auto-generated method stub
-	
-}
-
-
-
-
-@Override
-public void keyPressed(KeyEvent e) {
-	char hi = generateRandomLetter();
-	label.setText(Character.toString(hi));
-	if(e.getSource()==hi) {
-		
+	char generateRandomLetter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
 	}
-	
-}
 
+	char currentLetter;
 
+	public void runner() {
+		panel.add(label);
+		frame.add(panel);
+		
+		frame.setVisible(true);
+		char hi = generateRandomLetter();
+		label.setText(Character.toString(hi));
+		frame.addKeyListener(this);
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		frame.pack();
 
+	}
 
-@Override
-public void keyReleased(KeyEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyChar() == label.getText().charAt(0)) {
+			System.out.println("");
+			panel.setBackground(Color.GREEN);
+		}
+
+		else {
+			panel.setBackground(Color.RED);
+		}
+
+		char hi = generateRandomLetter();
+		label.setText(Character.toString(hi));
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
