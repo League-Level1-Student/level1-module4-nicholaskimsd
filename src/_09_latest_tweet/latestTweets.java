4de594c97bad7fc63160a70e6d5ba184083b54b1
League Student <league@iMac-24.attlocal.net>
2019-08-18 17:27:47 -0700

    l

--- src/_09_latest_tweet/latestTweets.java
+++ src/_09_latest_tweet/latestTweets.java
@@ -1,5 +1,23 @@
 package _09_latest_tweet;
 
-public class latestTweets {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class latestTweets {
+	JTextField textfield = new JTextField();
+	JButton button = new JButton();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+
+	public void runner() {
+		frame.add(panel);
+		panel.add(label);
+		frame.add(textfield);
+		frame.add(button);
+		
+	}
 }
--- src/_09_latest_tweet/latestTweetsrunner.java
+++ src/_09_latest_tweet/latestTweetsrunner.java
@@ -1,5 +1,9 @@
 package _09_latest_tweet;
 
 public class latestTweetsrunner {
-
+	public static void main(String[] args) {
+		
+	
+	new latestTweets().runner();
+}
 }
