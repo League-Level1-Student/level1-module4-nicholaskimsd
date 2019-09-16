e0a0b45911f4cfbcb8ee8c4f99606194e8021102
League Student <league@iMac-9.attlocal.net>
2019-09-15 17:24:45 -0700

    I did whack a mole IP

--- src/whackaMole.java
+++ src/whackaMole.java
@@ -1,40 +1,45 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class whackaMole {
+public class whackaMole implements ActionListener {
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JLabel label = new JLabel();
-	JTextField textfield = new JTextField();
+	
+	
 	JButton button = new JButton();
-	JTextField textfield1 = new JTextField();
+	
 
 	void drawButtons(int randumb) {
 
 		for (int i = 0; i < 30; i++) {
-			if(i == randumb) {
-				JButton coolButton = new JButton("mole!");
-			}
-			else {
-				JButton hi = new JButton();
-				
-			}
-				
+			JButton coolButton = new JButton();
+			coolButton.addActionListener(this);
+			panel.add(coolButton);
 
 		}
 	}
 
 	public void runner() {
 		frame.add(panel);
-		panel.add(label);
-		panel.add(textfield);
-		panel.add(textfield1);
-		panel.add(button);
+		Random randy = new Random(); 
+		int moleLocation = randy.nextInt(30);
+	drawButtons(moleLocation);
+		frame.setVisible(true);
+		frame.setSize(300, 300);
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
