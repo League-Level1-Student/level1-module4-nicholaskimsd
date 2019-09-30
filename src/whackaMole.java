6d257331541e06397d97905c7e4ebe89b3cd0bca
League Student <league@iMac-10.attlocal.net>
2019-09-29 17:25:14 -0700

    i finished whack a mole

--- src/whackaMole.java
+++ src/whackaMole.java
@@ -1,10 +1,14 @@
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -12,13 +16,17 @@ public class whackaMole implements ActionListener {
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	
-	
-	JButton button = new JButton();
+    Date timeAtStart = new Date();
+	int i = 0;
+
 	
 
 	void drawButtons(int randumb) {
 		
+		frame = new JFrame();
+		panel = new JPanel();
+		
+		System.out.println(randumb);
 		for (int i = 0; i < 30; i++) {
 			JButton coolButton = new JButton();
 			coolButton.addActionListener(this);
@@ -27,30 +35,58 @@ public class whackaMole implements ActionListener {
 			}
 			panel.add(coolButton);
 			
-		}
+		}	
+				frame.add(panel);
+		
+		frame.setVisible(true);
+		frame.setSize(500, 500);
 	}
 
-	public void runner() {
-		frame.add(panel);
-		Random randy = new Random(); 
-		int moleLocation = randy.nextInt(30);
-	drawButtons(moleLocation);
-	button.setText("mole");
-		frame.setVisible(true);
-		frame.setSize(300, 300);
-		
-	}
 
 	@Override
-	public void actionPerformed(ActionEvent f) {
-		JButton pressed = (JButton) f.getSource();
+	public void actionPerformed(ActionEvent e) {
+		JButton pressed = (JButton) e.getSource();
 		if(pressed.getText().equals("mole")) {
+			playSound("Applause.wav");
+			
 			frame.dispose();
+			
+			Random Rando = new Random(); 
+			int moleLocation = Rando.nextInt(30);
+			
+			drawButtons(moleLocation);		
+			
+			frame.setVisible(true);
+			frame.setSize(500, 500);
+			i += 1;
+			
+			
 		}
 		else {
-			
+			speak("words");
 			}
+		if(i==10) {
+			endGame(timeAtStart, 10);
 		}
+		}
+	void speak(String words) {
+	     try {
+	          Runtime.getRuntime().exec("say " + words).waitFor();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	private void endGame(Date timeAtStart, int molesWhacked) {
+	     Date timeAtEnd = new Date();
+	     JOptionPane.showMessageDialog(null, "Your whack rate is "
+	          + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	          + " moles per second.");
+	}
+	private void playSound(String fileName) {
+	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+	     sound.play();
+	}
+
 	
 		}
 	
--- src/whackaMoleRunner.java
+++ src/whackaMoleRunner.java
@@ -3,7 +3,7 @@ public class whackaMoleRunner {
 public static void main(String[] args) {
 		
 		
-		new whackaMole().runner();
+		new whackaMole().drawButtons(0);
 	}
 	}
 
